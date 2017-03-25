e6b48e45cfb478efdd86eca7463bebd6f20327be
Joseph Boyd <jcboyd@josephs-macbook-pro.home>
2017-03-25 16:59:44 +0100

    improved pyplot code

--- plot_confusion_matrix.py
+++ plot_confusion_matrix.py
@@ -1,37 +1,42 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
 import numpy as np
-from pylab import *
+import matplotlib.pyplot as plt
 
-matrix = np.array([[1,2,3], [4,5,6], [7,8,9]])
-labels = ['a', 'b', 'c']
 
-proportions = [1. * row / sum(row) for row in matrix]
+def plot_confusion_matrix(matrix, labels, title='Confusion matrix'):
+    fig, ax  = plt.subplots()
+    ax.set_xticks([x for x in range(len(labels))])
+    ax.set_yticks([y for y in range(len(labels))])
+    # Place labels on minor ticks
+    ax.set_xticks([x + 0.5 for x in range(len(labels))], minor=True)
+    ax.set_xticklabels(labels, rotation='90', fontsize=10, minor=True)
+    ax.set_yticks([y + 0.5 for y in range(len(labels))], minor=True)
+    ax.set_yticklabels(labels[::-1], fontsize=10, minor=True)
+    # Hide major tick labels
+    ax.tick_params(which='major', labelbottom='off', labelleft='off')
+    # Finally, hide minor tick marks
+    ax.tick_params(which='minor', width=0)
 
-figure()
-# Keep major ticks labeless
-xticks(range(len(labels)), [])
-yticks(range(len(labels)), [])
-# Place labels on minor ticks
-gca().set_xticks([x + 0.5 for x in range(len(labels))], minor=True)
-gca().set_xticklabels(labels, rotation='90', fontsize=10, minor=True)
-gca().set_yticks([y + 0.5 for y in range(len(labels))], minor=True)
-gca().set_yticklabels(labels[::-1], fontsize=10, minor=True)
-# Finally, hide minor tick marks...
-gca().tick_params('both', width=0, which='minor')
-# Plot heat map
-pcolor(array(proportions[::-1]), cmap=cm.Blues)
-# Plot counts as text
-for y in range(len(matrix)):
-    for x in range(len(matrix[y])):
-        if matrix[::-1][y][x] != 0:
-            text(x + 0.5, y + 0.5, matrix[::-1][y][x],
-                 fontsize=9,
-                 horizontalalignment='center',
-                 verticalalignment='center')
+    # Plot heat map
+    proportions = [1. * row / sum(row) for row in matrix]
+    ax.pcolor(np.array(proportions[::-1]), cmap=plt.cm.Blues)
 
-grid(True)
-title('Confusion matrix')
-tight_layout()
-show()
+    # Plot counts as text
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            confusion = matrix[::-1][row][col]
+            if confusion != 0:
+                ax.text(col + 0.5, row + 0.5, confusion, fontsize=9,
+                    horizontalalignment='center',
+                    verticalalignment='center')
+
+    # Add finishing touches
+    ax.grid(True, linestyle=':')
+    ax.set_title(title)
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    matrix = np.random.randint(0, 9, (10, 10))
+    labels = ['airplane','automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    plot_confusion_matrix(matrix, labels)
